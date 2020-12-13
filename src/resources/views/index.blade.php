fed0fbf619a6cb2122035c84051fb648f1f40745
M9snikFeed <den4ic2001@gmail.com>
2020-12-14 01:13:26 +0300

    Fix JS and CSS loading

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <title>MiniBlog</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
 </head>
 <body>
 
@@ -17,6 +17,6 @@
         <router-view></router-view>
     </div>
 
-<script src="./js/app.js"></script>
+<script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
