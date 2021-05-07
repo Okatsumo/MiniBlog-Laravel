bef56ca48a484ba338065089d28f32ff84cc0df4
M9snikFeed <den4ic2001@gmail.com>
2021-05-07 18:36:49 +0300

    Добавил комментарии

---
+++ src/app/Events/loadCommentsEven.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class loadCommentsEven implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $comment;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('comments.'.$this->comment->article_id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'loadCommentsEven';
+    }
+
+    public function broadcastWith(){
+        return [
+            'comment'=>$this->comment,
+            'author'=>$this->comment->author
+        ];
+    }
+}
